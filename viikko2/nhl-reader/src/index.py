fe5610295a6d4eb2693b018da37bceda52e4e969
Hanna Varkki <h.varkki@gmail.com>
2024-11-07 08:35:02 +0200

    tehtävä 10

---
+++ viikko2/nhl-reader/src/index.py
@@ -0,0 +1,24 @@
+import requests
+from player import Player
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
+    response = requests.get(url).json()
+
+    #print("JSON-muotoinen vastaus:")
+    #print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(player_dict)
+        players.append(player)
+
+    print("Players from FIN:")
+
+    for player in players:
+        if player.nationality == 'FIN':
+            print(player)
+
+if __name__ == "__main__":
+    main()
