f87b4fd8ab5dc1f9b0a8dc4e329414cf1828276d
Hanna Varkki <h.varkki@gmail.com>
2024-11-27 13:23:47 +0200

    Viikko 5 Tehtävä 6

--- viikko5/laskin/src/kayttoliittyma.py
+++ viikko5/laskin/src/kayttoliittyma.py
@@ -6,19 +6,20 @@ class Komento(Enum):
     SUMMA = 1
     EROTUS = 2
     NOLLAUS = 3
-    #KUMOA = 4
+    KUMOA = 4
 
 
 class Kayttoliittyma:
     def __init__(self, sovelluslogiikka, root):
         self._sovelluslogiikka = sovelluslogiikka
         self._root = root
+        self._viimeisin_komento = None
 
         self._komennot = {
             Komento.SUMMA: Summa(sovelluslogiikka, self._lue_syote),
             Komento.EROTUS: Erotus(sovelluslogiikka, self._lue_syote),
             Komento.NOLLAUS: Nollaus(sovelluslogiikka, self._lue_syote),
-            #Komento.KUMOA: Kumoa(sovelluslogiikka, self._lue_syote)
+            Komento.KUMOA: Kumoa(sovelluslogiikka, None, lambda: self._viimeisin_komento)
         }
 
     def kaynnista(self):
@@ -62,11 +63,16 @@ class Kayttoliittyma:
         self._kumoa_painike.grid(row=2, column=3)
 
     def _lue_syote(self):
-        return self._syote_kentta.get()
+        syote = self._syote_kentta.get()
+        return syote if syote.isdigit() else "0"
 
     def _suorita_komento(self, komento):
         komento_olio = self._komennot[komento]
         komento_olio.suorita()
+
+        if komento != Komento.KUMOA:
+            self._viimeisin_komento = komento_olio
+
         self._kumoa_painike["state"] = constants.NORMAL
 
         if self._sovelluslogiikka.arvo() == 0:
@@ -77,30 +83,60 @@ class Kayttoliittyma:
         self._syote_kentta.delete(0, constants.END)
         self._arvo_var.set(self._sovelluslogiikka.arvo())
 
+    def _kumoa(self):
+        if self._viimeisin_komento:
+            self._viimeisin_komento.kumoa()
+            self._viimeisin_komento = None
+            self._arvo_var.set(self._sovelluslogiikka.arvo())
+            self._kumoa_painike["state"] = constants.DISABLED
+
 class Summa:
     def __init__(self, sovelluslogiikka, syote_funktio):
         self._sovelluslogiikka = sovelluslogiikka
         self._syote_funktio = syote_funktio
+        self._edellinen_arvo = 0
 
     def suorita(self):
+        self._edellinen_arvo = self._sovelluslogiikka.arvo()
         arvo = int(self._syote_funktio())
         self._sovelluslogiikka.plus(arvo)
+
+    def kumoa(self):
+        self._sovelluslogiikka.aseta_arvo(self._edellinen_arvo)
 
 class Erotus:
     def __init__(self, sovelluslogiikka, syote_funktio):
         self._sovelluslogiikka = sovelluslogiikka
         self._syote_funktio = syote_funktio
+        self._edellinen_arvo = 0
 
     def suorita(self):
+        self._edellinen_arvo = self._sovelluslogiikka.arvo()
         arvo = int(self._syote_funktio())
         self._sovelluslogiikka.miinus(arvo)
+
+    def kumoa(self):
+        self._sovelluslogiikka.aseta_arvo(self._edellinen_arvo)
 
 class Nollaus:
     def __init__(self, sovelluslogiikka, _):
         self._sovelluslogiikka = sovelluslogiikka
+        self._edellinen_arvo = 0
 
     def suorita(self):
+        self._edellinen_arvo = self._sovelluslogiikka.arvo()
         self._sovelluslogiikka.nollaa()
 
+    def kumoa(self):
+        self._sovelluslogiikka.aseta_arvo(self._edellinen_arvo)
 
+class Kumoa:
+    def __init__(self, sovelluslogiikka, _, viimeisin_komento_funktio):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._viimeisin_komento_funktio = viimeisin_komento_funktio
+
+    def suorita(self):
+        komento = self._viimeisin_komento_funktio()
+        if komento:
+            komento.kumoa()
 
